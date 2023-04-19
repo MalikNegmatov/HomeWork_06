d11e65c66dd13aecb656c40449691c228aa7af7b
MalikNegmatov <maliknegmatov@gmail.com>
2023-04-19 12:29:16 +0300

    Инициализация проекта Домашнее задание урок Циклы часть 2.

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+
+    }
+
+    public static void task2 () {
+        System.out.println("\nЗадача 2");
+        // Пишем код для задачи 2
+
+    }
+
+    public static void task3 () {
+        System.out.println("\nЗадача 3");
+        // Пишем код для задачи 3
+
+    }
+
+    public static void task4 () {
+        System.out.println("\nЗадача 4");
+        // Пишем код для задачи 4
+
+    }
+
+    public static void task5 () {
+        System.out.println("\nЗадача 5");
+        // Пишем код для задачи 5
+
+    }
+    public static void task6 () {
+        System.out.println("\nЗадача 6");
+        // Пишем код для задачи 6
+
+    }
+    public static void task7 () {
+        System.out.println("\nЗадача 7");
+        // Пишем код для задачи 7
+    }
+
+    public static void task8 () {
+        System.out.println("\nЗадача 8");
+        // Пишем код для задачи 8
+    }
+}
